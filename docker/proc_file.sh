3ccc00866e7a30f036057a36e9ca219612a8b71f
ziningying <ziningying@163.com>
2021-06-12 09:36:57 +0800

    Update proc_file.sh

--- docker/proc_file.sh
+++ docker/proc_file.sh
@@ -12,7 +12,7 @@ if [ ! $CRZAY_JOY_COIN_ENABLE ]; then
    eval $(ps -ef | grep "jd_crazy_joy_coin" | grep -v "grep" | awk '{print "kill "$1}')
    echo '' >/scripts/logs/jd_crazy_joy_coin.log
    $CMD /scripts/jd_crazy_joy_coin.js | ts >>/scripts/logs/jd_crazy_joy_coin.log 2>&1 &
-   echo "默认jd_crazy_joy_coin重启完成"
+   echo "默认jd_crazy_joy_coin重启完成 "
 else
    if [ $CRZAY_JOY_COIN_ENABLE = "Y" ]; then
       echo "配置启用jd_crazy_joy_coin，杀掉jd_crazy_joy_coin任务，并重启"
